09bbcfc12732269b66c06eb8bc2586031dc21f1c
Zash60 <xath06@gmail.com>
2025-11-18 19:42:21 +0000

    Parar de rastrear firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyCHGM4Bw4w-3SM3ScVCLnLhcCI0ISTB7S0",
-  authDomain: "sm64-mobile-leaderboard.firebaseapp.com",
-  projectId: "sm64-mobile-leaderboard",
-  storageBucket: "sm64-mobile-leaderboard.firebasestorage.app",
-  messagingSenderId: "474371344168",
-  appId: "1:474371344168:web:a888b644a2a0b04ec9f21d"
-};
